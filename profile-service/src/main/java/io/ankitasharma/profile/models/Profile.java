9218db72255dfa20471df1f118c9b939deb968ee
ankita-sharma123 <73066234+ankita-sharma123@users.noreply.github.com>
2021-02-06 23:59:08 +0530

    Update Profile.java

--- profile-service/src/main/java/io/ankitasharma/profile/models/Profile.java
+++ profile-service/src/main/java/io/ankitasharma/profile/models/Profile.java
@@ -1,4 +1,4 @@
-package io.interviewready.profile.models;
+package io.ankitasharma.profile.models;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
