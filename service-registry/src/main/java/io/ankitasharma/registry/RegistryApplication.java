16ba6440ad850fcf7a9bfcb5f850ac16beb798f8
ankita-sharma123 <73066234+ankita-sharma123@users.noreply.github.com>
2021-02-07 12:39:07 +0530

    Update RegistryApplication.java

--- service-registry/src/main/java/io/ankitasharma/registry/RegistryApplication.java
+++ service-registry/src/main/java/io/ankitasharma/registry/RegistryApplication.java
@@ -1,4 +1,4 @@
-package io.interviewready.registry;
+package io.ankitasharma.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
